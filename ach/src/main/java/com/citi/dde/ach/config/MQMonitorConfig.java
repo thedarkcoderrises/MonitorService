612678695ea20a5793813e47e629143819f04aed
sony <sony@VAIO>
2017-09-02 21:21:56 +0530

    updated

--- ach/src/main/java/com/citi/dde/ach/config/MQMonitorConfig.java
+++ ach/src/main/java/com/citi/dde/ach/config/MQMonitorConfig.java
@@ -4,27 +4,19 @@ import java.util.Collection;
 
 import com.citi.dde.common.monitor.IMonitorConfig;
 import com.citi.dde.common.util.CountryCode;
+import com.citi.dde.common.util.DDEConstants;
 import com.citi.dde.common.util.Strategy;
 
 public class MQMonitorConfig implements IMonitorConfig {
 
 	@Override
 	public CountryCode getCountryCode() {
-		return CountryCode.getCountry(System.getProperty("country"));
+		return CountryCode.getCountry(System.getProperty(DDEConstants.COUNTRY));
 	}
 
 	@Override
 	public Collection<Strategy> getStrategyType() {
-		return Strategy.getStrategies(System.getProperty("app"));
+		return Strategy.getStrategies(System.getProperty(DDEConstants.APP));
 	}
 
-	@Override
-	public String[] getTypeIDs() {
-		return null;
-	}
-
-	@Override
-	public String getSpecialIndicator() {
-		return null;
-	}
 }
